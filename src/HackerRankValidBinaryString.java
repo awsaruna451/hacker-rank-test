9638a977a61ff8f5c4c4c5d2d86c2a8f28de572b
Administrator <administrator@Administrators-MacBook-Pro.local>
2022-07-14 22:59:10 +0800

    initial commit

---
+++ src/HackerRankValidBinaryString.java
@@ -0,0 +1,19 @@
+public class HackerRankValidBinaryString {
+
+    public static int minimumMoves(String s, int d) {
+        // Write your code here
+        int count = 0;
+        for(int x = 0; x < s.length(); x++) {
+           if (x > 0) {
+
+           }
+
+        }
+        return count;
+    }
+
+    public static void main(String ar[]) {
+        System.out.println(minimumMoves("0001", 2));
+    }
+
+}
